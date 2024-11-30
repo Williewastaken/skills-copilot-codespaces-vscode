b6167313864d5d71f1ac12ccd319f73e48cc432a
Willie Denman <188650603+Williewastaken@users.noreply.github.com>
2024-11-30 07:52:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,85 @@
+function skillsMember() {
+    var skills = {
+        name: 'John',
+        age: 32,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 2000
+    };
+
+    var skills2 = {
+        name: 'Jane',
+        age: 28,
+        skills: ['HTML', 'CSS', 'JS', 'React'],
+        salary: 3000
+    };
+
+    var skills3 = {
+        name: 'Mike',
+        age: 35,
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Node'],
+        salary: 4000
+    };
+
+    var skills4 = {
+        name: 'Sara',
+        age: 26,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 2000
+    };
+
+    var skills5 = {
+        name: 'David',
+        age: 28,
+        skills: ['HTML', 'CSS', 'JS', 'React'],
+        salary: 3000
+    };
+
+    var skills6 = {
+        name: 'Anna',
+        age: 35,
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Node'],
+        salary: 4000
+    };
+
+    var skills7 = {
+        name: 'Alex',
+        age: 26,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 2000
+    };
+
+    var skills8 = {
+        name: 'Eva',
+        age: 28,
+        skills: ['HTML', 'CSS', 'JS', 'React'],
+        salary: 3000
+    };
+
+    var skills9 = {
+        name: 'Tom',
+        age: 35,
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Node'],
+        salary: 4000
+    };
+
+    var skills10 = {
+        name: 'Sara',
+        age: 26,
+        skills: ['HTML', 'CSS', 'JS'],
+        salary: 2000
+    };
+
+    var skills11 = {
+        name: 'David',
+        age: 28,
+        skills: ['HTML', 'CSS', 'JS', 'React'],
+        salary: 3000
+    };
+
+    var skills12 = {
+        name: 'Anna',
+        age: 35,
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Node'],
+        salary: 7000
+    };
+}
